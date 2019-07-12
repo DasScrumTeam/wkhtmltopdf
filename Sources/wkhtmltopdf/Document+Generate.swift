27415e508de7bbfd7391fbdb942e4c2b38a54362
nichdu <mail@appfel.dev>
2019-07-12 10:09:52 +0200

    Anpassungen

--- Sources/wkhtmltopdf/Document+Generate.swift
+++ Sources/wkhtmltopdf/Document+Generate.swift
@@ -3,46 +3,43 @@ import Service
 
 extension Document {
 
-    public func generatePDF(on container: Container) throws -> Future<Data> {
-        let sharedThreadPool = try container.make(BlockingIOThreadPool.self)
+    public func generatePDF() throws -> Data {
+        let fileManager = FileManager.default
+        // Create the temp folder if it doesn't already exist
+        let workDir = "/tmp/vapor-wkhtmltopdf"
+        try fileManager.createDirectory(atPath: workDir, withIntermediateDirectories: true)
+        // Save input pages to temp files, and build up args to wkhtmltopdf
+        var wkArgs: [String] = [
+            "--zoom", self.zoom,
+            "--quiet",
+            "-s", self.paperSize,
+            "-T", "\(self.topMargin)mm",
+            "-R", "\(self.rightMargin)mm",
+            "-B", "\(self.bottomMargin)mm",
+            "-L", "\(self.leftMargin)mm",
+            "-O", self.orientation,
+        ]
 
-        return sharedThreadPool.runIfActive(eventLoop: container.eventLoop) { () -> Data in
-            let fileManager = FileManager.default
-            // Create the temp folder if it doesn't already exist
-            let workDir = "/tmp/vapor-wkhtmltopdf"
-            try fileManager.createDirectory(atPath: workDir, withIntermediateDirectories: true)
-            // Save input pages to temp files, and build up args to wkhtmltopdf
-            var wkArgs: [String] = [
-                "--zoom", self.zoom,
-                "--quiet",
-                "-s", self.paperSize,
-                "-T", "\(self.topMargin)mm",
-                "-R", "\(self.rightMargin)mm",
-                "-B", "\(self.bottomMargin)mm",
-                "-L", "\(self.leftMargin)mm",
-            ]
-
-            let pageFiles: [String] = try self.pages.map { page in
-                let name = UUID().uuidString + ".html"
-                let filename = "\(workDir)/\(name)"
-                try page.content.write(to: URL(fileURLWithPath: filename))
-                return filename
-            }
-            defer {
-                try? pageFiles.forEach(fileManager.removeItem)
-            }
-
-            wkArgs += pageFiles
-            // Call wkhtmltopdf and retrieve the result data
-            let wk = Process()
-            let stdout = Pipe()
-            wk.launchPath = "/usr/local/bin/wkhtmltopdf"
-            wk.arguments = wkArgs
-            wk.arguments?.append("-") // output to stdout
-            wk.standardOutput = stdout
-            wk.launch()
-            let pdf = stdout.fileHandleForReading.readDataToEndOfFile()
-            return pdf
+        let pageFiles: [String] = try self.pages.map { page in
+            let name = UUID().uuidString + ".html"
+            let filename = "\(workDir)/\(name)"
+            try page.content.write(to: URL(fileURLWithPath: filename))
+            return filename
         }
+        defer {
+            try? pageFiles.forEach(fileManager.removeItem)
+        }
+
+        wkArgs += pageFiles
+        // Call wkhtmltopdf and retrieve the result data
+        let wk = Process()
+        let stdout = Pipe()
+        wk.launchPath = "/usr/local/bin/wkhtmltopdf"
+        wk.arguments = wkArgs
+        wk.arguments?.append("-") // output to stdout
+        wk.standardOutput = stdout
+        wk.launch()
+        let pdf = stdout.fileHandleForReading.readDataToEndOfFile()
+        return pdf
     }
 }
