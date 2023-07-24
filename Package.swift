79f398fcebfddd002221195cb097195bbe28c5cb
andreasschliep <as@dasscrumteam.com>
2023-07-24 19:53:43 +0200

    Revert

--- Package.swift
+++ Package.swift
@@ -12,13 +12,22 @@ let package = Package(
             targets: ["wkhtmltopdf"]),
     ],
     dependencies: [
+<<<<<<< HEAD
 	.package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"), 
    ],
+=======
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/service.git", from: "1.0.0")
+    ],
+>>>>>>> parent of 830aef1 (Update Package.swift)
     targets: [
         .target(
             name: "wkhtmltopdf",
             dependencies: [
 		.product(name: "Vapor", package: "vapor"), // Updated to "vapor"
             ]),
+        .testTarget(
+            name: "wkhtmltopdfTests",
+            dependencies: ["wkhtmltopdf", "Vapor"]),
     ]
 )
