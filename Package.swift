6ec719b9818025a2c8233617717ff432c05c1200
andreasschliep <as@dasscrumteam.com>
2023-07-24 19:59:09 +0200

    Reset

--- Package.swift
+++ Package.swift
@@ -1,37 +1,21 @@
-<<<<<<< HEAD
-// swift-tools-version:5.5
-=======
-// swift-tools-version:4.2
->>>>>>> parent of f5160ba (Update Package.swift)
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-   name: "wkhtmltopdf",
-   platforms: [
-      .macOS(.v10_15)
-   ],
-   products: [
+    name: "wkhtmltopdf",
+    products: [
         .library(
             name: "wkhtmltopdf",
             targets: ["wkhtmltopdf"]),
     ],
     dependencies: [
-<<<<<<< HEAD
-	.package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"), 
-   ],
-=======
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/service.git", from: "1.0.0")
     ],
->>>>>>> parent of 830aef1 (Update Package.swift)
     targets: [
         .target(
             name: "wkhtmltopdf",
             dependencies: [
-		.product(name: "Vapor", package: "vapor"), // Updated to "vapor"
+                "Service"
             ]),
-        .testTarget(
-            name: "wkhtmltopdfTests",
-            dependencies: ["wkhtmltopdf", "Vapor"]),
     ]
 )
